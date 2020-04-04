2dcc7f0ddeb642bcc17b28ea8907314a2fa01839
Peter <pitrens.studio@gmail.com>
2020-04-05 00:13:48 +0800

    create item list in table

---
+++ app/javascript/components/home/Table/Item.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Thumbnail from "./Thumbnail";
+import styled from "styled-components";
+
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  cursor: pointer;
+  border-radius: 0;
+  font-weight: bold;
+  background: #fff;
+  color: #333 !important;
+  padding: 10px 20px;
+  font-size: 18px;
+  box-shadow: 0px 0px 0px 3px #473228, -6px 6px #ef5f17,
+    -6px 6px 0px 3px #473228;
+`;
+
+const Item = (props) => {
+  return (
+    <div className="row pt-4 pb-4">
+      <div className="col-md-10 offset-md-1">
+        <div className=" card px-5">
+          <div className="row">
+            <div className="col-md-4">
+              <Thumbnail />
+            </div>
+            <div className="col-md-8">
+              <div className="pt-4 pb-4">
+                <h4>{props.title}</h4>
+                <div>{props.description}</div>
+                <div className="cta-wrapper">
+                  <Button className="btn cta-btn"> Watch this video</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
