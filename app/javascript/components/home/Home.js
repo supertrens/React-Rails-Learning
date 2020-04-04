475539b5acc10a02cb35969031906d5a6623d516
Peter <pitrens.studio@gmail.com>
2020-04-04 23:36:23 +0800

    done with Jumbotron component

---
+++ app/javascript/components/home/Home.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Jumbotron from "./Jumbotron";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      course_modules: [
+        {
+          id: 1,
+          title: "This is the title 1",
+          description: "Lorem Ipsen",
+          active: false,
+        },
+        {
+          id: 2,
+          title: "This is the title 1 2",
+          description: "Lorem Ipsen",
+          active: false,
+        },
+        {
+          id: 3,
+          title: "This is the title 1 3",
+          description: "Lorem Ipsen",
+          active: false,
+        },
+        {
+          id: 4,
+          title: "This is the title 1 4",
+          description: "Lorem Ipsen",
+          active: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Jumbotron />
+      </div>
+    );
+  }
+}
+
+export default Home;
